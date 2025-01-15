2907aa92e3db6f4cc9aa5de4eb9766e6672de3e0
rahmatraywhite <rahmat.hidayat@raywhite.co.id>
2025-01-15 11:05:06 +0700

    add hourly temp

--- src/components/hourly-tempertaure.tsx
+++ src/components/hourly-tempertaure.tsx
@@ -1,7 +1,7 @@
 
 import type { ForecastData } from "../api/types"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis, Tooltip } from "recharts"
 import { format } from "date-fns"
 
 interface HourlyTemperatureProps {
@@ -40,6 +40,35 @@ const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
                 tickFormatter={(value) => `${value}°`}
               />
               {/* tooltip */}
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="rounded-lg border bg-background p-2 shadow-sm">
+                        <div className="grid grid-cols-2 gap-2">
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              Suhu
+                            </span>
+                            <span className="font-bold">
+                              {payload[0].value}°
+                            </span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              Terasa seperti
+                            </span>
+                            <span className="font-bold">
+                              {payload[1].value}°
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                }}
+              />
 
               <Line
                 type="monotone"
@@ -47,6 +76,14 @@ const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
                 stroke="#2563eb"
                 strokeWidth={2}
                 dot={false}
+              />
+              <Line
+                type="monotone"
+                dataKey={"feels_like"}
+                stroke="#2563eb"
+                strokeWidth={2}
+                dot={false}
+                strokeDasharray={"5 5"}
               />
 
             </LineChart>
